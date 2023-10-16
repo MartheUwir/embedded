7f2fc2279e7c36025a9020e17dccd5bb44a6d063
MartheUwir <marthuwir@gmail.com>
2023-10-16 21:36:52 +0200

    database conencted

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const app = express();
-// const port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3009;
+const cors = require("cors");
+const db = require("./db");
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(require("cors")());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-// const patientData = [];
+app.post("/api/patient", (req, res) => {
+  const patientInfo = req.body;
 
-// app.post("/api/patient", (req, res) => {
-//   const patientInfo = req.body;
-//   console.log(patientInfo);
-//   patientData.push(patientInfo);
-//   console.log({patientData});
-//   res.status(201).json({ message: "Patient data received and stored." });
-// });
+  // Insert patient data into the database
+  const stmt = db.prepare("INSERT INTO patients (PatientName, NationalID, HeartRate, BodyTemperature, FrequentSickness) VALUES (?, ?, ?, ?, ?)");
+  stmt.run(
+    patientInfo.PatientName,
+    patientInfo.NationalID,
+    patientInfo.HeartRate,
+    patientInfo.BodyTemperature,
+    patientInfo.FrequentSickness
+  );
+  stmt.finalize();
 
-// app.get("/api/patients", (req, res) => {
-//   res.json(patientData);
-// });
+  res.status(201).json({ message: "Patient data received and stored." });
+});
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.get("/api/patients", (req, res) => {
+  // Retrieve patient data from the database
+  db.all("SELECT * FROM patients", (err, rows) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    res.json(rows);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
