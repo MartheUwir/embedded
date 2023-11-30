c24f28bd2463a1bb7f44a11ae7f820c690162672
MartheUwir <marthuwir@gmail.com>
2023-11-30 11:23:43 +0200

    second assignment

--- sassign/retrieveData.js
+++ sassign/retrieveData.js
@@ -1,5 +1,4 @@
 const sqlite3 = require('sqlite3').verbose();
-const { StandardScaler } = require('ml-preprocessing'); // Import the StandardScaler
 const db = new sqlite3.Database('regression.db');
 
 let features = [];
@@ -8,9 +7,10 @@ let labels = [];
 db.serialize(() => {
     db.all(`SELECT age, cholesterol, bp FROM regression`, (err, rows) => {
         if (err) {
-            console.error(err.message);
-            return;
+          console.error(err.message);
+          return;
         }
+    
 
         rows.forEach((row) => {
             const age = parseInt(row.age);
@@ -18,17 +18,18 @@ db.serialize(() => {
             const bp = parseInt(row.bp);
 
             let labelValue;
-            if (bp > 140) {
-                labelValue = 'high';
-            } else if (bp > 120) {
-                labelValue = 'medium';
-            } else {
-                labelValue = 'low';
-            }
+          if (bp > 140) {
+            labelValue = 'high';
+          } else if (bp > 120) {
+            labelValue = 'medium';
+          } else {
+            labelValue = 'low';
+          }
 
             features.push([age, cholesterol]);
             labels.push(labelValue);
-        });
+          });
+
 
         const splitIndex = Math.floor(0.8 * features.length);
         const trainFeatures = features.slice(0, splitIndex);
@@ -36,22 +37,16 @@ db.serialize(() => {
         const trainLabels = labels.slice(0, splitIndex);
         const testLabels = labels.slice(splitIndex);
 
-        // Create a scaler instance and scale the features
-        const scaler = new StandardScaler();
-        scaler.fit(trainFeatures);
-        const scaledTrainFeatures = scaler.transform(trainFeatures);
-        const scaledTestFeatures = scaler.transform(testFeatures);
-
         // Display the split data
         console.log('Training Features:');
-        console.log(scaledTrainFeatures);
+        console.log(trainFeatures);
         console.log('Training Labels:');
         console.log(trainLabels);
         console.log('Testing Features:');
-        console.log(scaledTestFeatures);
+        console.log(testFeatures);
         console.log('Testing Labels:');
         console.log(testLabels);
-    });
+  });
 });
 
 db.close();
